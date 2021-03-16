26f34e37f2d674e8f2717587423d8e75c2ff1559
lukej2680 <50853238+lukej2680@users.noreply.github.com>
2021-03-16 16:43:37 -0400

    Incorporate API
    Added an API call to the "/" and to the "/search" route.
    Left console.logs in to demenstrate results from the API calls. Have commented out the res.json calls that should be used to send the needed data to the front end.
    Added the "/search" route just as an added capability of the site. It takes a request from the client containing the 'ticker' key and quaries the API for the full name, market open price and profit margin. Hardcoded to "AMRN" right now for demenstration purposes. Feel free to delete if uneeded.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const unirest = require("unirest");
 const express = require("express");
 const app = express();
 
@@ -7,7 +8,66 @@ const port = 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
+  var request = unirest("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-trending-tickers");
 
+  request.query({
+    "region": "US"
+  });
+
+  request.headers({
+    "x-rapidapi-key": "7812700e0emshebb9de4a28ebf3cp183990jsn341a8981b93a",
+    "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+    "useQueryString": true
+  });
+
+  request.end(function (response) {
+    if (res.error) throw new Error(res.error);
+    
+    var stocks = [];
+    for (var i = 0; i < 10; i++) {
+      var stock = []
+      var quote = response.body.finance.result[0].quotes[i];
+      var name = quote.shortName;
+      var price = quote.regularMarketPrice;
+      var ticker = quote.symbol;
+
+      stock.push(name);
+      stock.push(price);
+      stock.push(ticker);
+      stocks.push(stock);
+      console.log(stock);
+    }
+    console.log(stocks);
+    //res.json({"topTenTrendingStocks": stocks});
+  });
+});
+
+app.get("/search", function (req, res) {
+  //let ticker = req.query.ticker;
+  let ticker = "AMRN";
+
+  var req = unirest("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary");
+
+  req.query({
+    "symbol": ticker
+  });
+
+  req.headers({
+    "x-rapidapi-key": "7812700e0emshebb9de4a28ebf3cp183990jsn341a8981b93a",
+    "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+    "useQueryString": true
+  });
+
+
+  req.end(function (response) {
+    if (response.error) throw new Error(res.error);
+
+    let name = response.body.price.shortName;
+    let price = response.body.price.regularMarketOpen.fmt;
+    let margin = response.body.defaultKeyStatistics.profitMargins.fmt;
+    //res.json({"tickerName": name, "tickerMarketOpen": price, "tickerProfitMargin": margin});
+    console.log(name, price, margin);
+  });
 });
 
 app.listen(port, hostname, () => {
