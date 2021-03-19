b6289e99059532047af20cd7e109a7dd1dad1d0f
santrasatrajit <santrasatrajit@gmail.com>
2021-03-18 22:38:18 -0400

    Added the Stocks list

--- client/src/components/Stocks.js
+++ client/src/components/Stocks.js
@@ -1,26 +1,39 @@
 
 
 import React,{useEffect,useState} from 'react'
+import { List, Loader } from 'semantic-ui-react'
+
 
 function Stocks() {
   const [state,setState]=useState({
     data:null
   })
 
-useEffect(() => {
+  useEffect(() => {
 
-  fetch('http://localhost:3001/')
-  .then(response => response.json())
-  .then(json => setState({data:json}))
+    fetch('http://localhost:3001/')
+    .then(response => response.json())
+    .then(json => setState({data:json}))
 
-}, [])
+  }, [])
 
 
   return (
-    <div>
-  {state.data?<p>{JSON.stringify(state.data)}</p>:"Loading"}
-      
-    </div>
+    <div style={{paddingTop:10}}>
+      {state.data?
+        <List divided relaxed>
+          {state.data.topTenTrendingStocks.map(item => {
+            return (<List.Item>
+              <List.Content>
+                <List.Header>{item[0]}</List.Header>
+                <List.Description>{item[1]} {item[2]}</List.Description>
+              </List.Content>
+            </List.Item>)
+          })}
+        </List>
+      : <Loader active inline='centered' />
+      }
+  </div>
   )
 }
 
